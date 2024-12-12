b62bdf06fb7dcf4d7bbb4327bb00b8948fc98d08
SpanellisEvangelos <it22097@hua.gr>
2024-12-12 13:49:27 +0200

    mergeQuantities

--- src/main/java/gr/hua/dit/oop2/Main.java
+++ src/main/java/gr/hua/dit/oop2/Main.java
@@ -62,9 +62,29 @@ public class Main {
         );
     }
 
-    // Συνδυασμός ποσοτήτων (π.χ., "3 eggs" + "2 eggs")
+    // Συνδυασμός ποσοτήτων (π.χ. 500%gr + 125%gr )
     public static String mergeQuantities(String q1, String q2) {
-        return q1 + " + " + q2;
+        // Διαχωρισμός της ποσότητας και της μονάδας
+        String[] parts1 = q1.split("%");
+        String[] parts2 = q2.split("%");
+
+        if (parts1.length == 2 && parts2.length == 2 && parts1[1].equals(parts2[1])) {
+            // Αν οι μονάδες είναι ίδιες (π.χ. "gr")
+            try {
+                int quantity1 = Integer.parseInt(parts1[0]);
+                int quantity2 = Integer.parseInt(parts2[0]);
+
+                // Συνδυάζουμε τις ποσότητες
+                int totalQuantity = quantity1 + quantity2;
+                return totalQuantity + " " + parts1[1];
+            } catch (NumberFormatException e) {
+                // Αν δεν μπορούμε να μετατρέψουμε τις ποσότητες σε αριθμούς
+                return q1 + " + " + q2;
+            }
+        } else {
+            // Αν οι μονάδες είναι διαφορετικές ή το σχήμα δεν είναι σωστό
+            return q1 + " + " + q2;
+        }
     }
     
 
